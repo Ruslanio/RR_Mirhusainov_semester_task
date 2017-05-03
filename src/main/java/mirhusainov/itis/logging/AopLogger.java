b0cf0c2b5d0eabd7853335d7b37acddc7d64a925
Ruslan <mirhusainow@gmail.com>
2017-05-03 21:28:34 +0300

    AOP logging added

---
+++ src/main/java/mirhusainov/itis/logging/AopLogger.java
@@ -0,0 +1,47 @@
+package mirhusainov.itis.logging;
+
+import mirhusainov.itis.entities.CountriesEntity;
+import mirhusainov.itis.entities.UsersEntity;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+
+/**
+ * Created by Ruslan on 03.05.2017.
+ */
+
+@Aspect
+public class AopLogger {
+
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Before("execution(* mirhusainov.itis.controllers.AdminController.add*(..))")
+    public void addLog(){
+        logger.info("admin added something");
+    }
+
+    @Before("execution(* mirhusainov.itis.controllers.AdminController.delete*(..))")
+    public void delLog(){
+        logger.info("admin deleted something");
+    }
+
+    @After("execution(* mirhusainov.itis.service.UserService.add(..))")
+    public void userAddedLog(JoinPoint point){
+        BasicConfigurator.configure();
+        UsersEntity user = (UsersEntity) point.getArgs()[0];
+        logger.info("user " + user.getUserName() + "is added to the base");
+    }
+
+    @After("execution(* mirhusainov.itis.service.CountryService.add(..))")
+    public void CountryAddedLog(JoinPoint point){
+        BasicConfigurator.configure();
+        CountriesEntity country = (CountriesEntity) point.getArgs()[0];
+        logger.info("country " + country.getName() + "is added to the base");
+
+    }
+
+}
